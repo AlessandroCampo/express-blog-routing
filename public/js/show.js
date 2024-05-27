8fd2eda8c84306d5b1c0f650df70097bf32d5db8
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-27 18:00:36 +0200

    feat: dark mode on show

--- public/js/show.js
+++ public/js/show.js
@@ -14,18 +14,18 @@ const printCard = async () => {
         console.log(post);
         if (post) {
             const cardTemplate = `
-    <div class="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+    <div class="max-w-4xl mx-auto bg-gray-800 shadow-lg rounded-lg overflow-hidden text-white">
         <div class="px-6 py-4">
-            <p class="text-gray-600 text-sm"><span class="font-bold me-2">${post.author}</span>${formatDate(post.creation_date)}</p>
+            <p class="text-gray-400 text-sm"><span class="font-bold me-2">${post.author}</span>${formatDate(post.creation_date)}</p>
         </div>
         <img class="h-64 w-full object-cover" src="${post.image}" alt="${post.title}">
         <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">${post.title}</div>
-            <p class="text-gray-700 text-base mb-6">${post.content}</p>
-            <a href="${post.image_url}" class="text-blue-600 font-bold mt-4 text-lg" target="_blank">Image Link</a>
+            <p class="text-gray-300 text-base mb-6">${post.content}</p>
+            <a href="${post.image_url}" class="text-blue-400 font-bold mt-4 text-lg" target="_blank">Image Link</a>
         </div>
         <div class="px-6 py-4">
-            ${post.tags.map(tag => `<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">${tag}</span>`).join('')}
+            ${post.tags.map(tag => `<span class="inline-block bg-gray-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">${tag}</span>`).join('')}
         </div>
         <a href="http://localhost:3000/posts/${post.slug}/download" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Download this image
@@ -33,11 +33,12 @@ const printCard = async () => {
         <div class="px-6 py-4">
             <h3 class="text-xl font-bold mb-2">Comments</h3>
             <ul class="list-disc list-inside">
-                ${post.comments.map(comment => `<li class="text-gray-700">${comment}</li>`).join('')}
+                ${post.comments.map(comment => `<li class="text-gray-300">${comment}</li>`).join('')}
             </ul>
         </div>
     </div>
 `;
+
 
             cardHTML.innerHTML = cardTemplate;
         } else {
